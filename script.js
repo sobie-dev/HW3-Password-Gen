eff760e9d751c7ec70a971da33442c513f9a160d
Stephen Obie <sobie31@outlook.com>
2021-04-17 21:48:26 -0400

    logic working for number and upper

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 
 var generateBtn = document.querySelector("#generate");
 var enter;
-var confirmNum;
-var confirmChar;
-var confirmUpper;
-var confirmLower;
+// var confirmNum;
+// var confirmChar;
+// var confirmUpper;
+// var confirmLower;
 var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-var number = ["0","1","2","3","4","5","6","7","8","9"]
+var number = ["0","1","2","3","4","5","6","7","8","9",]
 var symbol = ["!","@","#","$","%","^","&","*","(",")","-","+","'","/",";",":","<",">","="]
 
-var combineChar = ["upperCase", "lowerCase", "number", "symbol"]
+var combineChar = [];
+var newPassword = "";
 
 // Write password to the #password input
 function writePassword() {
@@ -37,11 +38,22 @@ function generatePassword() {
   } else {
       // Continues once user input is validated
       confirmNum = confirm("Will this contain numbers?");
+      if (confirmNum) {
+        combineChar = number;
+      }
+      console.log(combineChar);
       confirmUpper = confirm("Will this contain Uppercase letters?");
+      if (confirmUpper) {
+        combineChar = combineChar.concat(upperCase);
+      }
+      console.log(combineChar);
       confirmLower = confirm("Will this contain Lowercase letters?");
       confirmChar = confirm("Will this contain special characters?");
+      console.log(confirmNum, confirmChar, confirmLower, confirmUpper)
+      for (let index = 0; index < enter; index++) {
+        newPassword = newPassword + combineChar[Math.floor(Math.random() * combineChar.length)];
+      }
   };
+  return newPassword;
 }
-// for (let i = 0, n = combineChar.length; i < String; ++i){
-//   newPassword += combineChar.
-// }
+
